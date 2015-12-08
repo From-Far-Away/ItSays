17e91afad39f279b166d90adfc350380acf00f10
Jonathan 'Slaals' Serra <jonathan.serra@utt.fr>
2015-12-08 12:19:35 +0100

    Setup authentication and password hash > has to be finished

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,7 +1,7 @@
 var User = require('../models/user');
 
 module.exports = function(router) {
-	router.post('/users', function(req, res) {
+	router.post('/signup', function(req, res) {
 		var user = new User();
 		
 		user.login = req.body.login;
@@ -13,6 +13,16 @@ module.exports = function(router) {
 			}
 
 			res.json({ message: 'User created!'} );
+		});
+	});
+
+	router.get('/signin', function(req, res) {
+		User.findOne({ login: req.body.login }, function(err, user) {
+			if(user.authenticate(req.body.password)) {
+				res.json({ message: 'connected' });
+			} else {
+				res.json({ message: 'oups' });
+			}
 		});
 	});
 }
--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,36 @@
 var mongoose = require('mongoose');
+var hashMethod = require('crypto-js/sha256');
+var crypto = require('crypto');
 
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-	login: String,
-	password: String
+var UserSchema = new Schema({
+	login: { type: String, unique: true, index: true, required: true },
+	password: { type: String, required: true },
+	salt: String
 });
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	if(!user.isModified('password')) {
+		return next();
+	}
+
+	user.salt = "" + crypto.randomBytes(16);
+	user.password = hashMethod(user.password + user.salt);
+	next();
+
+});
+
+UserSchema.authenticate = function(password, cb) {
+	var hash = hashMethod(password + this.salt);
+
+	if(hash === this.password) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+module.exports = mongoose.model('User', UserSchema);
